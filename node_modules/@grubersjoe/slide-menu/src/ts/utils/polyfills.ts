0f39ab4f33c0e7588c221358e1bba5b1fb4c266f
jacob-tucker <jacobtucker818@gmail.com>
2021-11-07 14:18:52 -0600

    remove node modules

--- node_modules/@grubersjoe/slide-menu/src/ts/utils/polyfills.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Polyfills for *drum roll* Internet Explorer 11
- */
-
-import 'custom-event-polyfill';
-
-if (!Element.prototype.matches) {
-  Element.prototype.matches = Element.prototype.msMatchesSelector;
-}
