bff4cf23a5f3c79b2d25b5c0473152ca29743685
gelicamarie <turla_angelica@yahoo.com>
2021-11-08 14:05:55 -0500

    feat: Welcome to Hyperhack box

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -1,15 +1,21 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import "./index.css"
+import Tribes from "./tribes/App"
 import App from "./tribes/App"
 // import App from './App';
 import reportWebVitals from "./reportWebVitals"
 
+
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path="/tribes" element={<Tribes />} />
+        <Route path="/" element={<App />} />
+      </Routes>
+
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root"),
